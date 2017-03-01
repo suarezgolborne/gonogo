c02d508576accd009b4c0d7d7da22230be9b6b3f
Sebastian Suarez-Golborne <sesu01@svt.se>
2017-03-01 14:53:03 +0100

    Zones

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import logo from './heyho.jpg';
 import './App.css';
 import SSGMap from './map.js';
+import { Accordion, AccordionItem } from 'react-sanfona';
 
 class App extends Component {
     componentDidUpdate() {
@@ -11,7 +12,65 @@ class App extends Component {
         console.log("cdm", this.state);
     }
 
+
   render() {
+
+    let zones = ["Vivalla, Örebro",
+"Hallunda/Norsborg, Botkyrka",
+"Husby, Stockholm",
+"Ronna/Geneta/Lina, Södertälje",
+"Rinkeby/Tensta, Stockholm",
+"Araby, Växjö",
+"Rosengård söder om Amiralsgatan, Malmö",
+"Södra Sofielund (Seved), Malmö",
+"Bergsjön, Göteborg",
+"Biskopsgården, Göteborg",
+"Gårdsten, Göteborg",
+"Hammarkullen",
+"Hjällbo",
+"Lövgärdet, Göteborg",
+"Skäggetorp, Linköping",
+"Oxhagen/Varberga, Örebro",
+"Tjärna Ängar, Borlänge",
+"Alby/Fittja, Botkyrka",
+"Tureberg, Sollentuna",
+"Adolfsberg/Dalhem/Drottninghög, Helsingborg",
+"Hässleholmen/Hulta, Borås",
+"Bäckby, Västerås",
+"Gottsunda/Valsätra, Uppsala",
+"Brandbergen, Haninge",
+"Bredäng, Stockholm",
+"Edsberg, Sollentuna",
+"Finnsta, Upplands Väsby",
+"Fornhöjden, Södertälje",
+"Hagsätra/Rågsved, Stockholm",
+"Hovsjö, Södertälje",
+"Hässelby/Vällingby, Stockholm",
+"Rissne/Hallonbergen, Sundbyberg",
+"Skogås, Huddinge",
+"Smedby, Upplands Väsby",
+"Sångvägen, Järfälla",
+"Smedby, Upplands Väsby",
+"Sångvägen, Järfälla",
+"Termovägen, Järfälla",
+"Älvsjö/Solberga, Stockholm",
+"Östberga, Stockholm",
+"Charlottesborg, Kristianstad",
+"Gamlegården, Kristianstad",
+"Holma/Kroksbäck/Bellevue, Malmö",
+"Koppargården, Landskrona",
+"Andersberg, Halmstad",
+"Falkagård, Falkenberg",
+"Hisings Backa, Göteborg",
+"Kronogården, Trollhättan",
+"Rannebergen, Göteborg",
+"Tynnered/Grevgården/Opaltorget, Västra Frölunda",
+"Fröslunda, Eskilstuna",
+"Hageby, Norrköping",
+"Lagersberg, Eskilstuna",
+"Råslätt, Jönköping",
+"Skiftinge, Eskilstuna"];
+
     return (
       <div className="App">
         <div className="App-header">
@@ -21,8 +80,22 @@ class App extends Component {
         <p className="App-intro">
             Fancy copy.
         </p>
-        <div><SSGMap />
-            </div>
+
+            <Accordion>
+                {zones.map((item) => {
+                    return (
+                        <AccordionItem title={`${ item }`} slug={item} key={item}>
+                            <div>
+                                {`Item ${ item } content`}
+                                {item === 3 ? <p><img src="https://cloud.githubusercontent.com/assets/38787/8015584/2883817e-0bda-11e5-9662-b7daf40e8c27.gif" /></p> : null}
+                            </div>
+                        </AccordionItem>
+                    );
+                })}
+            </Accordion>
+            <div><SSGMap />
+                </div>
+
       </div>
     );
   }
