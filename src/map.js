7b8f7301091f00d22d2e39016f00d69d20a8f7e0
Sebastian Suarez-Golborne <sesu01@svt.se>
2017-03-01 14:04:13 +0100

    hoho

--- src/map.js
+++ src/map.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { render } from 'react-dom';
 import { Map, Marker, Popup, TileLayer } from 'react-leaflet';
 
+// url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+
+
 class SSGMap extends React.Component {
 
     render() {
@@ -9,9 +12,12 @@ class SSGMap extends React.Component {
     const map = (
       <Map center={position} zoom={16} zoomControl={false} minZoom={16} maxZoom={16}>
         <TileLayer
-          url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+        url='https://api.mapbox.com/styles/v1/mapbox/light-v9/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1Ijoic3VhcmV6Z29sYm9ybmUiLCJhIjoiY2l6cXk4MXJkMDAxdzMzczdhZXNzNXN3bCJ9.nxxm26c-EUI5R_VtLHs6BQ'
+
           attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a>'
         />
+
+
         <Marker position={position}>
           <Popup>
             <span>Ett härligt tips.</span>
